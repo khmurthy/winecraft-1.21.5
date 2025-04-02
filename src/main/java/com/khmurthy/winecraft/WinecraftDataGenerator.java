2c2b21dd306d4185b6d98ec68205952f45e8f1ed
khmurthy <khmurthy6@gmail.com>
2025-04-02 02:04:28 -0400

    Initial commit

---
+++ src/main/java/com/khmurthy/winecraft/WinecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.khmurthy.winecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WinecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
